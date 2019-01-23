4006e17a98ae62f15dfa20389ff2f6641d65d925
Ryan.Peng <penghongyou@outlook.com>
2019-01-23 19:33:06 +0800

    全局跨域处理

---
+++ src/main/java/cn/com/taiji/collection/conf/WebMvcConfig.java
@@ -0,0 +1,23 @@
+package cn.com.taiji.collection.conf;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * 全局处理跨域问题
+ *
+ * @author penghongyou
+ */
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
+                .maxAge(3600)
+                .allowCredentials(true);
+    }
+}
