e403226c223487ac3fb16b7ec03dd49b3dfaaaf3
Ryan.Peng <penghongyou@outlook.com>
2019-01-28 09:43:01 +0800

    文娟案由案由树

--- src/main/java/cn/com/taiji/collection/service/DictService.java
+++ src/main/java/cn/com/taiji/collection/service/DictService.java
@@ -17,4 +17,6 @@ public interface DictService {
     List<Fydm> findAllFydm();
 
     List<Ajlx> findAllAjlx();
+
+    List<Ay> getChirldAy(List<Ay> ayList);
 }
--- src/main/java/cn/com/taiji/collection/service/DictServiceImpl.java
+++ src/main/java/cn/com/taiji/collection/service/DictServiceImpl.java
@@ -8,6 +8,7 @@ import cn.com.taiji.collection.mapper.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -43,5 +44,36 @@ public class DictServiceImpl implements DictService {
     @Override
     public List<Ajlx> findAllAjlx() {
         return ajlxMapper.selectAll();
+    }
+
+    @Override
+    public List<Ay> getChirldAy(List<Ay> ayList) {
+        List<Ay> ays = new ArrayList<>();
+        List<Ay> ayList2 = new ArrayList<>();
+        for(Ay ay1 : ayList){
+            if("0".equals(ay1.getAySjdm())){
+                ayList2.add(ay1);
+            }
+        }
+        for (Ay ay2 : ayList2){
+            ays.add(getAyInfo(ay2,ayList));
+        }
+        return ays;
+    }
+
+    private Ay getAyInfo(Ay ay,List<Ay> ayList){
+        List<Ay> ays = new ArrayList<>();
+        for(Ay ay1 : ayList){
+            if(ay.getAyDm().equals(ay1.getAySjdm())){
+                ays.add(ay1);
+            }
+        }
+        if(!ays.isEmpty()){
+            ay.setChirldAy(ays);
+            for(Ay ay2 : ays){
+                ay2 = getAyInfo(ay2,ayList);
+            }
+        }
+        return ay;
     }
 }
