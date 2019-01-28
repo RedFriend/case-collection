b5a17c9c33a42ad19a49cb4ca4058b7a090fe6a4
Ryan.Peng <penghongyou@outlook.com>
2019-01-28 09:40:52 +0800

    文娟案由案由树

--- src/main/java/cn/com/taiji/collection/controller/DictController.java
+++ src/main/java/cn/com/taiji/collection/controller/DictController.java
@@ -7,14 +7,18 @@ import cn.com.taiji.collection.entity.Fydm;
 import cn.com.taiji.collection.service.DictService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @RestController
 @RequestMapping("/dict")
+//@CrossOrigin(origins = "*",maxAge=3600)
 public class DictController extends BaseController {
 
     @Autowired
@@ -22,25 +26,31 @@ public class DictController extends BaseController {
 
     @GetMapping("/ay")
     @Cacheable(value = "ay")
-    public List<Ay> findAllAy() {
-        return dictService.findAllAy();
+    public List<Ay> findAllAy(HttpServletRequest request, HttpServletResponse response) {
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:8085");
+        List<Ay> ayList = dictService.findAllAy();
+        List<Ay> ayList2 = dictService.getChirldAy(ayList);
+        return ayList2;
     }
 
     @GetMapping("/ayAjlx")
     @Cacheable(value = "ayAjlx")
-    public List<AyAjlx> findAllAyAjlx() {
+    public List<AyAjlx> findAllAyAjlx(HttpServletRequest request, HttpServletResponse response) {
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:8085");
         return dictService.findAllAyAjlx();
     }
 
     @GetMapping("/fydm")
     @Cacheable(value = "fydm")
-    public List<Fydm> findAllFydm() {
+    public List<Fydm> findAllFydm(HttpServletRequest request, HttpServletResponse response) {
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:8085");
         return dictService.findAllFydm();
     }
 
     @GetMapping("/ajlx")
     @Cacheable(value = "ajlx")
-    public List<Ajlx> findAllAjlx() {
+    public List<Ajlx> findAllAjlx(HttpServletRequest request, HttpServletResponse response) {
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:8085");
         return dictService.findAllAjlx();
     }
 
